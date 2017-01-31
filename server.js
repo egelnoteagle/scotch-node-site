0cc84fe27c35370abff104d7ecc02219ff27eaee
Derek Egel <derekegel@cv15359.corp.classifiedventures.com>
2017-01-31 15:00:19 -0600

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+//require our dependencies
+var express = require('express');
+var app = express();
+var port = 8080;
+
+// route our app
+var router = require('./app/routes');
+app.use('/', router);
+
+//start the server
+app.listen(port, function() {
+    console.log('app started');
+});
